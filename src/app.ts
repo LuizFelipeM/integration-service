db52ad95f4604c41605389c3147e05f9ff25be3c
Luiz Moura <muniz.mourafelipe@gmail.com>
2024-10-26 17:55:20 -0300

    Add amqp-simple-client v2

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { server } from "./server"
 import { AppDataSource } from "./data-source";
 import { container } from "tsyringe";
 import * as rabbitMqSettings from "../rabbitmq.settings.json"
-import { Consumer, QueueBinding } from "amqp-simple-client"
+import { AmqpClient, QueueBinding, Subscription } from "amqp-simple-client"
 import { EventRepository } from "./repositories/event.repository";
 import { WebhookService } from "./services/webhook.service";
 
@@ -20,19 +20,30 @@ const start = async () => {
       routingKey: `${name}.events`
     }))
 
-    const consumer = new Consumer<unknown>(process.env.RABBITMQ_URL!, {
+    const client = new AmqpClient(process.env.RABBITMQ_URL!)
+    interface MyPayload {
+      id: number
+      name: string
+    }
+    const consumer = client.createConsumer<MyPayload>({
       name: rabbitMqSettings.webhook.queue.name,
       options: {
         durable: Boolean(rabbitMqSettings.webhook.queue.durable)
       },
-      bindings
+      // bindings
     })
 
-    const subscription = consumer.subscribe(({ headers, payload }) => {
-      if (headers?.event) {
-        const webhookService = container.resolve(WebhookService)
-        webhookService.process(headers.event, payload)
-      }
+    const subscription: Subscription<unknown>[] = []
+
+    server.delete("/unsubscribe", async () => subscription.forEach(s => s.unsubscribe()))
+    server.post("/subscribe", async () => {
+      subscription.push(
+        consumer.subscribe(async ({ headers, payload }) => {
+          if (headers?.event) {
+            const webhookService = container.resolve(WebhookService)
+            await webhookService.process(headers.event, payload)
+          }
+        }))
     })
 
     await server.listen({ port: 3000 })
